a7597c91a6f26ec4ebdb180d95326f19a12df01f
Thomas Graft <mr@thomasgraft.com>
2015-08-15 17:57:19 -0400

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# static-site-generator
+
+My static site generator.
+
+Fork from this to use.
+
+Features:
+
+- Gulp
+- Less
+- SWIG
+
+## Install
+
+Fork, clone, then:
+
+    $ npm install
+
+## Build
+
+To build `src/` directory into `dist/` directory, run:
+
+    $ gulp
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var swig = require('gulp-swig');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('default', function() {
+  // SWIG
+  gulp.src('src/*.swig')
+    .pipe(swig())
+    .pipe(gulp.dest('./dist/'))
+  // LESS
+  gulp.src('src/assets/less/*.less')
+    .pipe(less({
+      paths: [ 
+        path.join(__dirname, 'src', 'assets', 'less', 'includes'),
+      ]
+    }))
+    .pipe(gulp.dest('./dist/styles/'));
+});
